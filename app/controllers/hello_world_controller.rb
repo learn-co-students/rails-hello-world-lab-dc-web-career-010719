3b5524dc836c3ce80d6b2b8c82d3eb8e8b3c4c2e
dcjones1 <dcejjones@gmail.com>
2019-02-01 11:12:40 -0500

    Done.

---
+++ app/controllers/hello_world_controller.rb
@@ -0,0 +1,9 @@
+class HelloWorldController < ApplicationController
+  # def index
+  #   #this will look for the index.erb view under hello world directory in views
+  #   # render "hello_world"
+  # end
+  def hello_world
+    #this will look for the hello_world.erb view under hello world directory in views
+  end
+end
